3520da79fbf33daa4510d86d4d87fb274221b030
bala-amypo <bala@amypo.in>
2025-12-17 18:11:51 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -13,6 +13,11 @@ public class User {
     private String role;
     private LocalDateTime createdAt;
    
-    public 
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
 
 }
