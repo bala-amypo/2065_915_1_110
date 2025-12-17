d8a38fd961e824df9a612049347e3453777aa31f
bala-amypo <bala@amypo.in>
2025-12-17 17:31:20 +0000

    User commit

--- src/main/java/com/example/demo/entity/Resource.java
+++ src/main/java/com/example/demo/entity/Resource.java
@@ -1,5 +1,14 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
 public class Resource {
-    
+    private Long id;
+    private String resourceName;
+    private String resourceType;
+    private int capacity;
+    private String location;
+    private LocalDateTime createdAt;
+
 }
