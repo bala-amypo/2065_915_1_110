4717959a3b6632a4e5246f3062d952c03da73b1e
bala-amypo <bala@amypo.in>
2025-12-17 17:41:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/Resource.java
+++ src/main/java/com/example/demo/entity/Resource.java
@@ -10,5 +10,43 @@ public class Resource {
     private int capacity;
     private String location;
     private LocalDateTime createdAt;
-
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getResourceName() {
+        return resourceName;
+    }
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+    public String getResourceType() {
+        return resourceType;
+    }
+    public void setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+   
+   
 }
