cb1ed730ae3cc07e67fe1822085086c9cf6b4554
bala-amypo <bala@amypo.in>
2025-12-20 07:56:27 +0000

    User commit

--- src/main/java/com/example/demo/service/AllocationRuleService.java
+++ src/main/java/com/example/demo/service/AllocationRuleService.java
@@ -4,12 +4,7 @@ import com.example.demo.entity.AllocationRule;
 import java.util.List;
 
 public interface AllocationRuleService {
-
     AllocationRule createRule(AllocationRule rule);
-
+    AllocationRule getRule(Long id);
     List<AllocationRule> getAllRules();
-
-    AllocationRule getRuleById(Long id);
-
-    void deleteRule(Long id);
 }
