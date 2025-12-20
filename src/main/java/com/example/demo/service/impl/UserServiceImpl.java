d671b48f21df8ccd1c3b256448a331706e4541e1
bala-amypo <bala@amypo.in>
2025-12-20 16:34:40 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -9,34 +9,35 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepo;
-    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepo = userRepository;
-    }
+            public UserServiceImpl(UserRepository userRepository) {
+                    this.userRepo = userRepository;
+                        }
 
-    @Override
-    public User registerUser(User user) {
-        if (userRepo.existsByEmail(user.getEmail())) {
-            throw new IllegalArgumentException("User already exists");
-        }
+                            @Override
+                                public User registerUser(User user) {
+                                        if (userRepo.existsByEmail(user.getEmail())) {
+                                                    throw new IllegalArgumentException("User already exists");
+                                                            }
 
-        if (user.getRole() == null) {
-            user.setRole("USER");
-        }
+                                                                    if (user.getRole() == null) {
+                                                                                user.setRole("USER");
+                                                                                        }
 
-        user.setPassword(encoder.encode(user.getPassword()));
-        return userRepo.save(user);
-    }
+                                                                                                user.setPassword(encoder.encode(user.getPassword()));
+                                                                                                        return userRepo.save(user);
+                                                                                                            }
 
-    @Override
-    public User getUser(Long id) {
-        return userRepo.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-    }
+                                                                                                                @Override
+                                                                                                                    public User getUser(Long id) {
+                                                                                                                            return userRepo.findById(id)
+                                                                                                                                            .orElseThrow(() -> new RuntimeException("User not found"));
+                                                                                                                                                }
 
-    @Override
-    public List<User> getAllUsers() {
-        return userRepo.findAll();
-    }
-}
+                                                                                                                                                    @Override
+                                                                                                                                                        public List<User> getAllUsers() {
+                                                                                                                                                                return userRepo.findAll();
+                                                                                                                                                                    }
+                                                                                                                                                                    }
+                                                                                                                                                                    
