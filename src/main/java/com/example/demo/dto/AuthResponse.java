76d17fe6c349c180a02216b5814ecbe51888e9b2
bala-amypo <bala@amypo.in>
2025-12-20 06:19:34 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,17 +1,32 @@
 package com.example.demo.dto;
 
 public class AuthResponse {
-    
-}
-package com.example.demo.dto;
 
-public class AuthRequest {
+    private String token;
+    private Long userId;
     private String email;
-    private String password;
+    private String role;
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public AuthResponse(String token, Long userId, String email, String role) {
+        this.token = token;
+        this.userId = userId;
+        this.email = email;
+        this.role = role;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getToken() {
+        return token;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
