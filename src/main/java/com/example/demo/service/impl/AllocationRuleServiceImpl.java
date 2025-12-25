deb964632b3c8b882b44b90d935fc7fbd7da89a2
bala-amypo <bala@amypo.in>
2025-12-25 12:02:58 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AllocationRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AllocationRuleServiceImpl.java
@@ -2,9 +2,11 @@ package com.example.demo.service.impl;
  
 import com.example.demo.entity.AllocationRule; 
 import com.example.demo.repository.AllocationRuleRepository; 
-import com.example.demo.service.AllocationRuleService; 
+import com.example.demo.service.AllocationRuleService;
+import org.springframework.stereotype.Service; 
 import java.util.List; 
  
+@Service
 public class AllocationRuleServiceImpl implements AllocationRuleService { 
  
     private final AllocationRuleRepository ruleRepo; 
