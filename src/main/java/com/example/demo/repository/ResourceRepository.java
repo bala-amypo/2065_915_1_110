d40297fad4f90a0eb6261770d3d880a98d6cb981
bala-amypo <bala@amypo.in>
2025-12-25 11:20:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/ResourceRepository.java
+++ src/main/java/com/example/demo/repository/ResourceRepository.java
@@ -1,13 +1,8 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Resource;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface ResourceRepository extends JpaRepository<Resource, Long> {
-    boolean existsByResourceName(String resourceName);
-    List<Resource> findByResourceType(String resourceType);
-}
+package com.example.demo.repository; 
+import com.example.demo.entity.Resource; 
+import org.springframework.data.jpa.repository.JpaRepository; 
+import java.util.List; 
+public interface ResourceRepository extends JpaRepository<Resource, Long> { 
+boolean existsByResourceName(String resourceName); 
+List<Resource> findByResourceType(String resourceType); 
+} 
