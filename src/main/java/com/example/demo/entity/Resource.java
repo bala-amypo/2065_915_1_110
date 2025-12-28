6be47229f1d057b7ab62cd53d9fe66d13fdbac52
bala-amypo <bala@amypo.in>
2025-12-28 05:17:48 +0000

    User commit

--- src/main/java/com/example/demo/entity/Resource.java
+++ src/main/java/com/example/demo/entity/Resource.java
@@ -1,30 +1,4 @@
-package com.example.demo.entity; 
- 
-import jakarta.persistence.*; 
-import java.time.LocalDateTime; 
-import java.util.List; 
- 
-@Entity 
-@Table(name = "resources") 
-public class Resource { 
- 
-    @Id 
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private Long id; 
- 
-    @Column(unique = true) 
-    private String resourceName; 
- 
-    private String resourceType; 
- 
-    private Integer capacity; 
- 
-    private String location; 
- 
-    private LocalDateTime createdAt; 
- 
-    @OneToMany(mappedBy = "resource") 
-    private List<ResourceAllocation> allocations; 
+t<ResourceAllocation> allocations; 
  
     public Resource() { 
         this.createdAt = LocalDateTime.now(); 
